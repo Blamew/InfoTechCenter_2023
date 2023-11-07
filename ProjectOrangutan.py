87d4265af9f2df841aed28ee5e97ee37170134e9
Bryson <bryson.lamew00@gmail.com>
2023-11-07 08:34:56 -0500

    Added gasLevelAlert function, Tested and Stable

--- ProjectOrangutan.py
+++ ProjectOrangutan.py
@@ -17,12 +17,30 @@ def listOfGasStations():
     gasStationsNearby = random.choice(gasStations)
     return gasStationsNearby
 
-
-
-
-
 #Function will call the gasLevelGauge to determine gas level and then find a close gas station if low
 def gasLevelAlert():
     milesToGasStationLow = round(random.uniform(1,25),1)
     milesToGasStationQuarterTank = round(random.uniform(25.1,50),1)
+    gasLevelIndicator = gasLevelGauge()
+    if gasLevelIndicator == "Empty":
+        print("***WARNING - YOU ARE ON EMPTY***")
+        sleep(1.25)
+        print("Calling Triple AAA")
+    elif gasLevelIndicator == "Low":
+        print("Your gas tank is extremely low, checking Google Maps for the closest gas station")
+        sleep(1.5)
+        print("The closest gas station is",listOfGasStations(),"which is",milesToGasStationLow,"miles away.")
+    elif gasLevelIndicator == "Quarter Tank":
+        print("You have a Quarter of a Tank of gasoline, cheching Google Maps for the closest gas station.")
+        sleep(1.5)
+        print("The closest gas station is",listOfGasStations(),"which is",milesToGasStationQuarterTank,"miles away")
+    elif gasLevelIndicator == "Half Tank":
+        print("Your gas tank has a half of a tank left which is plenty to get to your destinations.")
+    else:
+        print("Your gas tank is full - Yeah! - Congratulation! - Vroom Vroom!")
+
+
+
+
+gasLevelAlert()
 
